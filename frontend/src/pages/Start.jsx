c261593d53561b750a63978e53c68737407a23d7
Christopher4113 <ch.lam1328@gmail.com>
2025-03-28 17:36:10 -0400

    started the one page we need

---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,13 @@
+import {useState,useEffect} from 'react'
+import axios from 'axios';
+import '../styles/start.css';
+
+const Start = () => {
+  const [info, setInfo] = useState([]);
+  
+  return (
+    <div>Start</div>
+  )
+}
+
+export default Start
